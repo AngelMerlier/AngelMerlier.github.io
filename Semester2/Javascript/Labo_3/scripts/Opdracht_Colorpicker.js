f6779f828086e323fc1a3f65b710d6ad909c040c
AngelMerlier <93262875+AngelMerlier@users.noreply.github.com>
2022-03-09 16:38:30 +0100

    .

--- Semester2/Javascript/Labo_3/scripts/Opdracht_Colorpicker.js
+++ Semester2/Javascript/Labo_3/scripts/Opdracht_Colorpicker.js
@@ -1,3 +1,18 @@
 const setup = () => {
 }
+/*const() => colorSliders(){
+    btnInputEvents(red)
+    btnInputEvents(green)
+    btnInputEvents(blue)
+}
+
+const() => btnInputEvents(btn){
+    btn.addEventListener('input', () => {
+        displayColors()
+        initColorNumbrVals()
+        initSliderColors()
+        changeRangeNumVal()
+    })
+}*/
+
 window.addEventListener("load", setup);
