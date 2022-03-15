58e780824376710f358f8e47443838bacf2abc06
AngelMerlier <93262875+AngelMerlier@users.noreply.github.com>
2022-03-15 18:47:35 +0100

    colorpicker changes

--- Semester2/Javascript/Labo_3/scripts/Opdracht_Colorpicker.js
+++ Semester2/Javascript/Labo_3/scripts/Opdracht_Colorpicker.js
@@ -1,7 +1,27 @@
 const setup = () => {
     let slider = document.getElementById("slider");
-    for(let i = 0; i < slider)
+    for(let i = 0; i < sliders.length; i++)
+    {
+        sliders[i].addEventListener("change", update);
+        sliders[i].addEventListener("input", update);
+    }
 }
+
+const update = () => {
+    const sliders = document.getElementsByClassName("slider");
+    let kleurRood = sliders[0].value;
+    let kleurGroen = sliders[1].value;
+    let kleurBlauw = sliders[2].value;
+
+    let display = document.getElementById("display");
+
+    labels[0].textContent = 'red: ' + kleurRood;
+    labels[1].textContent = 'green: ' + kleurGroen;
+    labels[2].textContent = 'blue: ' + kleurBlauw;
+
+    display.style.backgroundColor= `rgb(${kleurRood},${kleurGroen}, ${kleurBlauw})`
+}
+
 /*const() => colorSliders(){
     btnInputEvents(red)
     btnInputEvents(green)
