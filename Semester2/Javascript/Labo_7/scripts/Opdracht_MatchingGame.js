323da9ac7d98ac52efed961efb7dd72e170ded9c
AngelMerlier <93262875+AngelMerlier@users.noreply.github.com>
2022-04-24 11:56:54 +0200

    foutcode inspecteren in chrome

--- Semester2/Javascript/Labo_7/scripts/Opdracht_MatchingGame.js
+++ Semester2/Javascript/Labo_7/scripts/Opdracht_MatchingGame.js
@@ -2,13 +2,17 @@ let AANTAL_HORIZONTAAL=4;
 let AANTAL_VERTICAAL=3;
 let AANTAL_KAARTEN=6;
 //css en html aanpassen***//
-let kaarten = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.png", "6.png", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.png", "6.png"]
+let kaarten = ["card1", "card2", "card3", "card4", "card5", "card6", "card1", "card2", "card3", "card4", "card5", "card6"]
 aantal_selected: 0;
 timeoutId: 0;
 
-const newCard = (index) => {
+/* kaarten zijn verdwenen zoek fout in code(inspecteren) zie appendchild*/
+const newCard = (idx) => {
     let card = document.createElement("img");
     card.classList.add("card");
+    let cardIdx = Math.round(Math.random() * (kaarten.length - 1));
+    card.classList.add(kaarten[cardIdx]);
+    kaarten.splice(cardIdx, 1);
     return card;
 }
 
@@ -16,9 +20,9 @@ const newGame = () => {
     //voor uitbreiding formule aanpassen met de 4,3 van horizontaal en verticaal bij globale waarden//
     for(let i = 0; i < 12; i++) {
         let card = newCard(i);
-        let rijX = (i%AANTAL_VERTICAAL) +1;
+        let rowIdx = (i%AANTAL_VERTICAAL) + 1;
 
-        document.getElementById(`rij${rijX}`).appendChild(card);
+        document.getElementById(`row${rowIdx}`).appendChild(card);
     }
 }
 
