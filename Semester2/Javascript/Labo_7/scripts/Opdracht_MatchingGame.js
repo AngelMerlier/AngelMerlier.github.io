ab675cab34390e0ae74fb03404c55627073a22c8
AngelMerlier <93262875+AngelMerlier@users.noreply.github.com>
2022-04-24 10:55:29 +0200

    foute benaming kaarten

--- Semester2/Javascript/Labo_7/scripts/Opdracht_MatchingGame.js
+++ Semester2/Javascript/Labo_7/scripts/Opdracht_MatchingGame.js
@@ -1,19 +1,19 @@
 let AANTAL_HORIZONTAAL=4;
 let AANTAL_VERTICAAL=3;
 let AANTAL_KAARTEN=6;
-
-let kaarten = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"]
+//css en html aanpassen***//
+let kaarten = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.png", "6.png", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.png", "6.png"]
 aantal_selected: 0;
 timeoutId: 0;
 
 const newCard = (index) => {
     let card = document.createElement("img");
     card.classList.add("card");
-
     return card;
 }
 
 const newGame = () => {
+    //voor uitbreiding formule aanpassen met de 4,3 van horizontaal en verticaal bij globale waarden//
     for(let i = 0; i < 12; i++) {
         let card = newCard(i);
         let rijX = (i%AANTAL_VERTICAAL) +1;
@@ -31,7 +31,7 @@ const setup = () => {
 const hideTheCards = () => {
     let kaarten = document.getElementsByClassName("card");
     for(let i= 0; cards.lengt; i++){
-        cards[i].src = 'images/back.png';
+        cards[i].src = 'images/backCard2.png';
         cards[i].addEventListener("click", turnAround);
     }
 }
